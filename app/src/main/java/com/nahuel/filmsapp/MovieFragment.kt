ec57d7e5cf49fdd82a843ab9989b507e1912725d
Nahue Imaz <nahuelji.773@gmail.com>
2022-03-31 20:37:15 -0300

    Se importaron las librerias correspondientes y se crearon los layouts principales

---
+++ app/src/main/java/com/nahuel/filmsapp/MovieFragment.kt
@@ -0,0 +1,16 @@
+package com.nahuel.filmsapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class MovieFragment : Fragment(R.layout.fragment_movie) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
