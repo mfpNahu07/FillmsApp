add5f730e5c376b7a107d9893d29a5f88473fcb8
Nahue Imaz <nahuelji.773@gmail.com>
2022-04-01 15:35:17 -0300

    Se agrego Binding al gradle app y a las clases

--- app/src/main/java/com/nahuel/filmsapp/MovieDetailFragment.kt
+++ app/src/main/java/com/nahuel/filmsapp/MovieDetailFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.nahuel.filmsapp.databinding.FragmentMovieDetailBinding
 
 
 class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
 
 
+    private lateinit var binding: FragmentMovieDetailBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieDetailBinding.bind(view)
     }
 
 }
--- app/src/main/java/com/nahuel/filmsapp/MovieFragment.kt
+++ app/src/main/java/com/nahuel/filmsapp/MovieFragment.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.nahuel.filmsapp.databinding.FragmentMovieBinding
 
 
 class MovieFragment : Fragment(R.layout.fragment_movie) {
 
+
+    private lateinit var binding : FragmentMovieBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieBinding.bind(view)
+
     }
 
 }
