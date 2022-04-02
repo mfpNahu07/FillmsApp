0f0f14c6106bbc4a4749f870e03ea3958c222eb0
Nahue Imaz <nahuelji.773@gmail.com>
2022-04-02 18:29:29 -0300

    Se creo la capa de datos

--- app/src/main/java/com/nahuel/filmsapp/data/model/Movie.kt
+++ app/src/main/java/com/nahuel/filmsapp/data/model/Movie.kt
@@ -1,4 +1,22 @@
 package com.nahuel.filmsapp.data.model
 
-data class Movie(val id: Int = -1) {
-}
+//Clase con los atributos que vienen desde la API.
+data class Movie(val adult: Boolean = false,
+                 val backdrop_path: String = "",
+                 val genre_ids: List<Int> = listOf(),
+                 val id: Int = -1,
+                 val original_language: String = "",
+                 val original_title: String = "",
+                 val overview: String = "",
+                 val popularity: Double = -1.0,
+                 val poster_path: String = "",
+                 val release_date: String = "",
+                 val title: String = "",
+                 val video: Boolean = false,
+                 val vote_average: Double = -1.0,
+                 val vote_count: Int = -1
+)
+
+//Clase que contiene una lista con los objetos que vienen desde la API, cuyo atributo
+// se llama igual que el array de objetos que tiene los resultados en la API.
+data class MovieList(val results: List<Movie> = listOf())
--- app/src/main/java/com/nahuel/filmsapp/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/nahuel/filmsapp/data/remote/MovieDataSource.kt
@@ -1,4 +1,21 @@
 package com.nahuel.filmsapp.data.remote
 
+import com.nahuel.filmsapp.data.model.Movie
+import com.nahuel.filmsapp.data.model.MovieList
+
 class MovieDataSource {
+
+    fun getUpcomingMovies(): MovieList {
+        return MovieList()
+    }
+
+
+    fun getTopRatedMovies(): MovieList {
+        return MovieList()
+    }
+
+    fun getPopularMovies(): MovieList {
+        return MovieList()
+    }
+
 }
