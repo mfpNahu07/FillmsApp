1d88742e043d07dac545f4b7ab6e50ac6a549527
Nahue Imaz <nahuelji.773@gmail.com>
2022-04-11 15:55:36 -0300

    Se creo la entidad que representa la tabla

--- app/src/main/java/com/nahuel/filmsapp/data/model/Movie.kt
+++ app/src/main/java/com/nahuel/filmsapp/data/model/Movie.kt
@@ -1,22 +1,60 @@
 package com.nahuel.filmsapp.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 //Clase con los atributos que vienen desde la API.
-data class Movie(val adult: Boolean = false,
-                 val backdrop_path: String = "",
-                 val genre_ids: List<Int> = listOf(),
-                 val id: Int = -1,
-                 val original_language: String = "",
-                 val original_title: String = "",
-                 val overview: String = "",
-                 val popularity: Double = -1.0,
-                 val poster_path: String = "",
-                 val release_date: String = "",
-                 val title: String = "",
-                 val video: Boolean = false,
-                 val vote_average: Double = -1.0,
-                 val vote_count: Int = -1
+data class Movie(
+    val adult: Boolean = false,
+    val backdrop_path: String = "",
+    val genre_ids: List<Int> = listOf(),
+    val id: Int = -1,
+    val original_language: String = "",
+    val original_title: String = "",
+    val overview: String = "",
+    val popularity: Double = -1.0,
+    val poster_path: String = "",
+    val release_date: String = "",
+    val title: String = "",
+    val video: Boolean = false,
+    val vote_average: Double = -1.0,
+    val vote_count: Int = -1
 )
 
 //Clase que contiene una lista con los objetos que vienen desde la API, cuyo atributo
 // se llama igual que el array de objetos que tiene los resultados en la API.
 data class MovieList(val results: List<Movie> = listOf())
+
+
+//Se usa para la bdd local
+@Entity
+data class MovieEntity(
+    @PrimaryKey
+    val id: Int = -1,
+    @ColumnInfo(name = "adult")
+    val adult: Boolean = false,
+    @ColumnInfo(name = "backdrop_path")
+    val backdrop_path: String = "",
+    //val genre_ids: List<Int> = listOf(),
+    @ColumnInfo(name = "original_language")
+    val original_language: String = "",
+    @ColumnInfo(name = "original_title")
+    val original_title: String = "",
+    @ColumnInfo(name = "overview")
+    val overview: String = "",
+    @ColumnInfo(name = "popularity")
+    val popularity: Double = -1.0,
+    @ColumnInfo(name = "poster_path")
+    val poster_path: String = "",
+    @ColumnInfo(name = "release_date")
+    val release_date: String = "",
+    @ColumnInfo(name = "title")
+    val title: String = "",
+    @ColumnInfo(name = "video")
+    val video: Boolean = false,
+    @ColumnInfo(name = "vote_average")
+    val vote_average: Double = -1.0,
+    @ColumnInfo(name = "vote_count")
+    val vote_count: Int = -1
+)
